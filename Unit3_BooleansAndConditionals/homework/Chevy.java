964a1a72de75cdc214be44f4f75bb998293eaebb
Alex Kim <90787108+Kim1563@users.noreply.github.com>
2021-09-23 11:59:48 -0500

    Update Chevy.java

--- Unit3_BooleansAndConditionals/homework/Chevy.java
+++ Unit3_BooleansAndConditionals/homework/Chevy.java
@@ -3,7 +3,7 @@ package Unit3_BooleansAndConditionals.homework;
 public class Chevy {
     private int year;
     private int mileage;
-    private int fuelEfficiency;
+    private double fuelEfficiency;
     private int price;
     private String model;
     private String color;
@@ -29,7 +29,7 @@ public class Chevy {
         this.sport = false;
     }
 
-    public Chevy(int year, int mileage, int fuelEfficiency, int price, String model, String color, boolean luxury, boolean fourWD, boolean sport) {
+    public Chevy(int year, int mileage, double fuelEfficiency, int price, String model, String color, boolean luxury, boolean fourWD, boolean sport) {
         this.year = year;
         this.mileage = mileage;
         this.fuelEfficiency = fuelEfficiency;
@@ -57,11 +57,11 @@ public class Chevy {
         this.mileage = mileage;
     }
 
-    public int getFuelEfficiency() {
+    public double getFuelEfficiency() {
         return fuelEfficiency;
     }
 
-    public void setFuelEfficiency(int fuelEfficiency) {
+    public void setFuelEfficiency(double fuelEfficiency) {
         this.fuelEfficiency = fuelEfficiency;
     }
 
@@ -111,6 +111,39 @@ public class Chevy {
 
     public void setSport(boolean sport) {
         this.sport = sport;
+    }
+
+    public int compareTo(Chevy other){
+        return (price - other.price);
+    }
+
+    public boolean equals(Chevy other){
+        Boolean thisIsNew = this.mileage < 200;
+        Boolean otherIsNew = other.mileage < 200;
+        return this.model.equals(other.model) &&
+                this.color.equals(other.color) &&
+                thisIsNew.equals(otherIsNew);
+    }
+
+    public String toString(){
+        String returnString = this.year + MAKE + this.model + "(" + this.color + ")";
+        returnString += "\tPRICE:\t\t$" + this.price;
+        returnString += "\tMILES:\t\t" + this.mileage;
+        returnString += "\tFUEL EFFICIENCY:\t\t" + this.fuelEfficiency + " mpg";
+        returnString += "\tPACKAGES:";
+        if (luxury){
+            returnString += "\t\t-Luxury Package";
+        }
+        if (fourWD){
+            returnString += "\t\t-4WD Package";
+        }
+        if (sport){
+            returnString += "\t\t-Sports Package";
+        }
+        if (!(luxury || fourWD || sport)){
+            returnString += "\t\t-None";
+        }
+        return returnString;
     }
 
 }
