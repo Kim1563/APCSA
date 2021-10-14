b811f00e307390a0509c93bd6424fe818123eb85
Alex Kim <90787108+Kim1563@users.noreply.github.com>
2021-10-14 17:26:08 -0500

    Finished PigLatin

--- Unit2_UsingObjects/homework/WS1/ShoeTester.java
+++
@@ -1,8 +0,0 @@
-package Unit2_UsingObjects.homework.WS1;
-
-public class ShoeTester {
-    public static void main(String[] args) {
-        Shoe Jordans = new Shoe("Air Jordans", 8, 160);
-
-    }
-}
