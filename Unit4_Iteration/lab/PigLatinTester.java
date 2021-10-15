72267cd66871f1ede8345a8c996d396e6d4cf638
Alex Kim <90787108+Kim1563@users.noreply.github.com>
2021-10-15 11:58:53 -0500

    Update PigLatinTester.java

--- Unit4_Iteration/lab/PigLatinTester.java
+++ Unit4_Iteration/lab/PigLatinTester.java
@@ -1,7 +1,16 @@
 package Unit4_Iteration.lab;
 
+import java.util.*;
+
 public class PigLatinTester {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String userResponse = "", userString = "";
+        while (!userResponse.equalsIgnoreCase("N")){
+            System.out.print("Enter a phrase: ");
+            userString = in.nextLine();
+            System.out.println("Your string: ");
+        }
 
         String phrase1 = "The black cat ate a red apple";
         String phrase2 = "Writing college essays is driving me crazy";
