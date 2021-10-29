c8bf5d1f193197a69756d69620bdcbb253be6822
Alex Kim <90787108+Kim1563@users.noreply.github.com>
2021-10-29 15:21:41 -0500

    Update Grade.java

--- Unit5_Classes/homework/PartnerProject/Grade.java
+++ Unit5_Classes/homework/PartnerProject/Grade.java
@@ -1,8 +1,20 @@
 package Unit5_Classes.homework.PartnerProject;
 
 public class Grade {
-    private double grades;
-    private String letterGrades;
+    private double grade;
+    private String letterGrade;
 
+    public Grade(double grade){
+        this.grade = grade;
+        letterGrade = setLetterGrade(grade);
+    }
     
+    private void setLetterGrade(double grade){
+        if (grade >= 98){
+            this.letterGrade = "A+";
+        } else if (grade >= 93 && grade < 98){
+            this.letterGrade = "A";
+        } else if (grade >= 90 && grade < 93){
+            this.letterGrade = "A-";
+        }
 }
