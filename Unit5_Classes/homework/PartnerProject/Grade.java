aa0445b62fa2f6c126f132ae1727a3b36ff73ede
Alex Kim <alexk747air@gmail.com>
2021-10-29 11:58:58 -0500

    Started Partner Project

---
+++ Unit5_Classes/homework/PartnerProject/Grade.java
@@ -0,0 +1,8 @@
+package Unit5_Classes.homework.PartnerProject;
+
+public class Grade {
+    private double grades;
+    private String letterGrades;
+
+    
+}
---
+++ Unit5_Classes/homework/PartnerProject/Student.java
@@ -0,0 +1,11 @@
+package Unit5_Classes.homework.PartnerProject;
+
+import java.util.*;
+
+public class Student {
+    private String name;
+    private int gradeLevel;
+    private ArrayList<Grade> grades;
+
+
+}
