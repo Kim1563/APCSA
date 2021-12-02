bfbef359acf01d6216e06c7a4fcc0cb4bac1dc69
Alex Kim <alexk747air@gmail.com>
2021-12-02 11:25:31 -0600

    Update TriviaGame.java

--- Unit6_Arrays/lab/TriviaGame.java
+++ Unit6_Arrays/lab/TriviaGame.java
@@ -1,10 +1,14 @@
 package Unit6_Arrays.lab;
 
+import java.util.*;
+
 public class TriviaGame {
-    private Question[] allQuestions;
+    private ArrayList<Question> allQuestions;
 
-    private static int streak;
-    private static int totalPointsEarned;
+    private static int streak = 0;
+    private static int totalPointsEarned = 0;
 
-
+    public TriviaGame(ArrayList<Question> allQuestions) {
+        this.allQuestions = allQuestions;
+    }
 }
